760b3ba679694a61971a44765e22179191275a8a
임수빈 <hange253@gmail.com>
2022-12-15 00:51:41 +0900

    [CREATE] spring > banner

---
+++ pages/spring.tsx
@@ -0,0 +1,19 @@
+import styled from "@emotion/styled";
+import Header from "../components/common/header";
+import Banner from "../components/spring/banner";
+
+export default function Home() {
+  return (
+    <Container>
+      <Header />
+      <Banner />
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+`;
