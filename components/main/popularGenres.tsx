ead90b9abd0b6b660b86c5c98a99db89fed90e31
임수빈 <hange253@gmail.com>
2023-03-08 14:17:15 +0900

    [REMOVE] file
    차트 수정

--- components/main/popularGenres.tsx
+++ components/main/popularGenres.tsx
@@ -5,54 +5,39 @@ import { useEffect, useRef } from "react";
 export default function PopularGenres() {
   const canvasEl = useRef<HTMLCanvasElement>(null);
 
-  let myLineChart: Chart<"line", number[], string>;
+  let myBarChart: Chart<"bar", number[], string>;
+
+  const temp = {};
 
   const createBarChart = () => {
     if (canvasEl && canvasEl.current) {
       const ctx = canvasEl.current.getContext("2d");
 
       if (ctx) {
-        myLineChart = new Chart(ctx, {
-          type: "line",
+        myBarChart = new Chart(ctx, {
+          type: "bar",
           data: {
-            labels: [
-              "발라드",
-              "댄스",
-              "랩/힙합",
-              "POP",
-              "인디음악",
-              "국내드라마",
-              "록/메탈",
-              "R&B/Soul",
-            ],
+            labels: ["발라드", "댄스", "랩/힙합", "POP", "록/메탈", "R&B/soul"],
             datasets: [
               {
-                label: "봄",
-                data: [1, 8, 0, 0, 0, 0, 0, 0],
-                borderColor: ["#FFA7BE"],
+                label: "봄 소비량",
+                data: [8, 1, 0, 0, 0, 0],
                 backgroundColor: ["#FFA7BE"],
-                tension: 0.1,
               },
               {
-                label: "여름",
-                data: [1, 6, 0, 1, 0, 0, 0, 1],
-                borderColor: ["#ff6b6b"],
-                backgroundColor: ["#ff6b6b"],
-                tension: 0.1,
+                label: "여름 소비량",
+                data: [1, 6, 0, 1, 0, 1],
+                backgroundColor: ["#FF6B6B"],
               },
               {
-                label: "가을",
-                data: [1, 3, 0, 1, 0, 0, 3, 0],
-                borderColor: ["#FFC212"],
+                label: "가을 소비량",
+                data: [1, 3, 0, 2, 3, 0],
                 backgroundColor: ["#FFC212"],
-                tension: 0.1,
               },
               {
-                label: "겨울",
-                data: [1, 1, 6, 0, 0, 0, 1, 0],
-                borderColor: ["#46467A"],
+                label: "겨울 소비량",
+                data: [1, 1, 6, 0, 1, 0],
                 backgroundColor: ["#46467A"],
-                tension: 0.1,
               },
             ],
           },
@@ -72,7 +57,7 @@ export default function PopularGenres() {
     createBarChart();
 
     return () => {
-      myLineChart.destroy();
+      myBarChart.destroy();
     };
   }, []);
 
